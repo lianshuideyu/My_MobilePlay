ca26fe13523c3fece4b6eb8fd8e565b32fcfffc7
lianshuideyu <834893479@qq.com>
2017-05-19 13:26:10 +0800

    底部工具栏布局

--- app/src/main/java/com/atguigu/my_mobileplay/MainMobilePlayActivity.java
+++ app/src/main/java/com/atguigu/my_mobileplay/MainMobilePlayActivity.java
@@ -2,12 +2,16 @@ package com.atguigu.my_mobileplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainMobilePlayActivity extends AppCompatActivity {
+    private RadioGroup rg_bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mobile_play);
+
+        rg_bottom = (RadioGroup)findViewById(R.id.rg_bottom);
     }
 }
---
+++ app/src/main/java/com/atguigu/my_mobileplay/fragment/BaseFragment.java
@@ -0,0 +1,49 @@
+package com.atguigu.my_mobileplay.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/5/19.
+ */
+
+public abstract class BaseFragment extends Fragment{
+    public Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return initView();
+    }
+
+    /**
+     * 这里有子view集成，实现子view的视图,必须继承
+     * @return
+     */
+    public abstract View initView();
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initData();
+    }
+
+    //添加数据
+    public void initData(){
+
+    }
+}
