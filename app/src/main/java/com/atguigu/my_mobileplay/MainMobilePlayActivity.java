0af6dbffc21e1d39c095eae75e1bf6aedd5f6b7b
lianshuideyu <834893479@qq.com>
2017-05-19 14:40:52 +0800

    解决切换Fragment切换导致重新创建Fragment问题

--- app/src/main/java/com/atguigu/my_mobileplay/MainMobilePlayActivity.java
+++ app/src/main/java/com/atguigu/my_mobileplay/MainMobilePlayActivity.java
@@ -1,7 +1,7 @@
 package com.atguigu.my_mobileplay;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioGroup;
@@ -19,6 +19,7 @@ public class MainMobilePlayActivity extends AppCompatActivity {
     private RadioGroup rg_bottom;
     private List<BaseFragment> fragments;
     private int position;
+    private Fragment temFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,20 +53,52 @@ public class MainMobilePlayActivity extends AppCompatActivity {
                 }
 
                 BaseFragment currFragment = fragments.get(position);//得到当前Fragment视图
+                addFragment(currFragment);
 
-                //1得到事务管理
-                FragmentManager fm = getSupportFragmentManager();
-                //2开启事务
-                FragmentTransaction ft = fm.beginTransaction();
-                //3替换
-                ft.replace(R.id.fl_pageFragment,currFragment);
-                //4提交
-                ft.commit();
+
             }
         });
         
         //默认勾选本地视频
         rg_bottom.check(R.id.rb_local_video);
+    }
+    
+    //切换页面Fragment
+    private void addFragment(BaseFragment currFragment) {
+        if(currFragment != temFragment) {
+            //得到事务管理并开启事务
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            if(!currFragment.isAdded()) {
+                //判断是否添加--如没有添加
+                //把之前的隐藏
+                if(temFragment != null) {
+                    ft.hide(temFragment);
+                }
+                //添加当前的
+                ft.add(R.id.fl_pageFragment,currFragment);
+                
+            }else {
+                //如添加
+                //把之前的隐藏
+                if(temFragment != null) {
+                    ft.hide(temFragment);
+                }
+                //显示当前的
+                ft.show(currFragment);
+            }
+            ft.commit();
+            //这部不要忘记，替换缓存的页面
+            temFragment = currFragment;
+        }
+        
+        /*//1得到事务管理
+        FragmentManager fm = getSupportFragmentManager();
+        //2开启事务
+        FragmentTransaction ft = fm.beginTransaction();
+        //3替换
+        ft.replace(R.id.fl_pageFragment,currFragment);
+        //4提交
+        ft.commit();*/
     }
 
     private void initDatas() {
