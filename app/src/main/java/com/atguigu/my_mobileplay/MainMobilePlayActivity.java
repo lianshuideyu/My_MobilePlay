71e7a78eb4a7fcb890ccee88606a565119d12c7f
lianshuideyu <834893479@qq.com>
2017-05-19 14:10:38 +0800

    Fragment页面之前的切换

--- app/src/main/java/com/atguigu/my_mobileplay/MainMobilePlayActivity.java
+++ app/src/main/java/com/atguigu/my_mobileplay/MainMobilePlayActivity.java
@@ -1,11 +1,24 @@
 package com.atguigu.my_mobileplay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioGroup;
+
+import com.atguigu.my_mobileplay.fragment.BaseFragment;
+import com.atguigu.my_mobileplay.page.LocalAudioFragment;
+import com.atguigu.my_mobileplay.page.LocalVideoFragment;
+import com.atguigu.my_mobileplay.page.NetAudioFragment;
+import com.atguigu.my_mobileplay.page.NetVideoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainMobilePlayActivity extends AppCompatActivity {
     private RadioGroup rg_bottom;
+    private List<BaseFragment> fragments;
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,54 @@ public class MainMobilePlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_mobile_play);
 
         rg_bottom = (RadioGroup)findViewById(R.id.rg_bottom);
+
+        initDatas();
+
+        initListener();
+    }
+
+    private void initListener() {
+        rg_bottom.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId) {
+                    case  R.id.rb_local_video:
+                        position = 0;
+                        break;
+                    case  R.id.rb_local_audio:
+                        position = 1;
+                        break;
+                    case  R.id.rb_net_audio:
+                        position = 2;
+                        break;
+                    case  R.id.rb_net_video:
+                        position = 3;
+                        break;
+                }
+
+                BaseFragment currFragment = fragments.get(position);//得到当前Fragment视图
+
+                //1得到事务管理
+                FragmentManager fm = getSupportFragmentManager();
+                //2开启事务
+                FragmentTransaction ft = fm.beginTransaction();
+                //3替换
+                ft.replace(R.id.fl_pageFragment,currFragment);
+                //4提交
+                ft.commit();
+            }
+        });
+        
+        //默认勾选本地视频
+        rg_bottom.check(R.id.rb_local_video);
+    }
+
+    private void initDatas() {
+        fragments = new ArrayList<>();
+
+        fragments.add(new LocalVideoFragment());
+        fragments.add(new LocalAudioFragment());
+        fragments.add(new NetAudioFragment());
+        fragments.add(new NetVideoFragment());
     }
 }
---
+++ app/src/main/java/com/atguigu/my_mobileplay/page/NetVideoFragment.java
@@ -0,0 +1,33 @@
+package com.atguigu.my_mobileplay.page;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.my_mobileplay.fragment.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/5/19.
+ */
+
+public class NetVideoFragment extends BaseFragment {
+    private TextView textView;
+
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        textView.setTextColor(Color.RED);
+        textView.setTextSize(25);
+        textView.setGravity(Gravity.CENTER);
+
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+
+        textView.setText("网络视频");
+    }
+}
