e3b1e6eecd3c6fef17ad5da84a47464d1814d494
lianshuideyu <834893479@qq.com>
2017-05-20 12:13:20 +0800

    视频的拖动暂停

--- app/src/main/java/com/atguigu/my_mobileplay/activity/SystemVideoPlayerActivity.java
+++ app/src/main/java/com/atguigu/my_mobileplay/activity/SystemVideoPlayerActivity.java
@@ -4,29 +4,117 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.MediaController;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.atguigu.my_mobileplay.R;
 
-public class SystemVideoPlayerActivity extends AppCompatActivity {
+public class SystemVideoPlayerActivity extends AppCompatActivity implements View.OnClickListener {
     private VideoView vv;
     private Uri uri;
+
+    private LinearLayout llTop;
+    private TextView tvName;
+    private ImageView ivBattery;
+    private TextView tvSystemTime;
+    private Button btnVoice;
+    private SeekBar seekbarVoice;
+    private Button btnSwitchPlayer;
+    private LinearLayout llBottom;
+    private TextView tvCurrentTime;
+    private SeekBar seekbarVideo;
+    private TextView tvDuration;
+    private Button btnExit;
+    private Button btnPre;
+    private Button btnStartPause;
+    private Button btnNext;
+    private Button btnSwitchScreen;
+
+    private void findViews() {
+        setContentView(R.layout.activity_system_video_player);
+        llTop = (LinearLayout) findViewById(R.id.ll_top);
+        tvName = (TextView) findViewById(R.id.tv_name);
+        ivBattery = (ImageView) findViewById(R.id.iv_battery);
+        tvSystemTime = (TextView) findViewById(R.id.tv_system_time);
+        btnVoice = (Button) findViewById(R.id.btn_voice);
+        seekbarVoice = (SeekBar) findViewById(R.id.seekbar_voice);
+        btnSwitchPlayer = (Button) findViewById(R.id.btn_switch_player);
+        llBottom = (LinearLayout) findViewById(R.id.ll_bottom);
+        tvCurrentTime = (TextView) findViewById(R.id.tv_current_time);
+        seekbarVideo = (SeekBar) findViewById(R.id.seekbar_video);
+        tvDuration = (TextView) findViewById(R.id.tv_duration);
+        btnExit = (Button) findViewById(R.id.btn_exit);
+        btnPre = (Button) findViewById(R.id.btn_pre);
+        btnStartPause = (Button) findViewById(R.id.btn_start_pause);
+        btnNext = (Button) findViewById(R.id.btn_next);
+        btnSwitchScreen = (Button) findViewById(R.id.btn_switch_screen);
+        vv = (VideoView) findViewById(R.id.vv);
+
+        btnVoice.setOnClickListener(this);
+        btnSwitchPlayer.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+        btnPre.setOnClickListener(this);
+        btnStartPause.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+        btnSwitchScreen.setOnClickListener(this);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_video_player);
-
-        vv = (VideoView)findViewById(R.id.vv);
+        //初始化控件
+        vv = (VideoView) findViewById(R.id.vv);
+        findViews();
 
         uri = getIntent().getData();
 
         //设置三个监听
         //当准备好播放时候调用
+        setListener();
+
+        //设置seekbar状态的监听
+        seekbarVideo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            /**
+             * 
+             * @param seekBar
+             * @param i
+             * @param b true:用户拖动改变的，false:系统更新改变的
+             */
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //如果拖动了
+                if(fromUser) {
+                    vv.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void setListener() {
         vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
+                //得到视频的总时长
+                int duration = vv.getDuration();
+                seekbarVideo.setMax(duration);
+
                 vv.start();//开始播放
             }
         });
@@ -50,6 +138,33 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
         //设置播放地址
         vv.setVideoURI(uri);
         //设置控制面板
-        vv.setMediaController(new MediaController(this));
+        //vv.setMediaController(new MediaController(this));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnVoice) {
+        } else if (v == btnSwitchPlayer) {
+
+        } else if (v == btnExit) {
+
+        } else if (v == btnPre) {
+
+        } else if (v == btnStartPause) {
+            if(vv.isPlaying()) {
+                vv.pause();//暂停
+                btnStartPause.setBackgroundResource(R.drawable.btn_start_selector);
+
+            }else {
+                //播放
+                vv.start();
+                btnStartPause.setBackgroundResource(R.drawable.btn_pause_selector);
+            }
+
+        } else if (v == btnNext) {
+
+        } else if (v == btnSwitchScreen) {
+
+        }
     }
 }
