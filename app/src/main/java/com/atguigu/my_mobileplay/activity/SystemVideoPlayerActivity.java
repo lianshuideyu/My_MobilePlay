e77825ce16254fdee5e8c3bfd7c3a6679f91b157
lianshuideyu <834893479@qq.com>
2017-05-21 18:04:02 +0800

    监听手机音量加减键，实现与视频音量同步

--- app/src/main/java/com/atguigu/my_mobileplay/activity/SystemVideoPlayerActivity.java
+++ app/src/main/java/com/atguigu/my_mobileplay/activity/SystemVideoPlayerActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -358,6 +359,27 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
 
         return super.onTouchEvent(event);
 
+    }
+
+    //监听手机音量加减按键
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            //音量减
+            currentVoice--;
+            updateVoiceProgress(currentVoice);
+            handler.removeMessages(HIDE_MEDIACONTROLLER);
+            handler.sendEmptyMessageDelayed(HIDE_MEDIACONTROLLER,4000);
+        }else if(keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            //音量加
+            currentVoice++;
+            updateVoiceProgress(currentVoice);
+            handler.removeMessages(HIDE_MEDIACONTROLLER);
+            handler.sendEmptyMessageDelayed(HIDE_MEDIACONTROLLER,4000);
+        }
+
+
+        return super.onKeyDown(keyCode, event);
     }
 
     /**
