15a7a25d6187e6cc7665d45c7ec5fbbc2c7fcde8
lianshuideyu <834893479@qq.com>
2017-05-30 17:48:34 +0800

    联网获取数据并解析

--- app/src/main/java/com/atguigu/my_mobileplay/page/NetAudioFragment.java
+++ app/src/main/java/com/atguigu/my_mobileplay/page/NetAudioFragment.java
@@ -7,7 +7,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.atguigu.my_mobileplay.R;
+import com.atguigu.my_mobileplay.adapter.NetAudioFragmentAdapter;
+import com.atguigu.my_mobileplay.domain.NetAudioBean;
 import com.atguigu.my_mobileplay.fragment.BaseFragment;
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.common.util.LogUtil;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,6 +28,7 @@ import butterknife.ButterKnife;
 
 public class NetAudioFragment extends BaseFragment {
 
+    private String url = "http://s.budejie.com/topic/list/jingxuan/1/budejie-android-6.2.8/0-20.json?market=baidu&udid=863425026599592&appname=baisibudejie&os=4.2.2&client=android&visiting=&mac=98%3A6c%3Af5%3A4b%3A72%3A6d&ver=6.2.8";
 
     @Bind(R.id.listview)
     ListView listview;
@@ -25,6 +36,8 @@ public class NetAudioFragment extends BaseFragment {
     ProgressBar progressbar;
     @Bind(R.id.tv_nomedia)
     TextView tvNomedia;
+
+    private NetAudioFragmentAdapter myAdapter;
 
     @Override
     public View initView() {
@@ -41,6 +54,58 @@ public class NetAudioFragment extends BaseFragment {
         Log.e("TAG", "NetAudioPager-initData");
         super.initData();
 
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        RequestParams reques = new RequestParams(url);
+        x.http().get(reques, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+
+                LogUtil.e("onSuccess联网成功==" + result);
+                processData(result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                LogUtil.e("onError联网失败==" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                LogUtil.e("onCancelled==" + cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                LogUtil.e("onFinished==");
+            }
+        });
+
+    }
+
+    /**
+     * 解析和显示数据
+     *
+     * @param json
+     */
+    private void processData(String json) {
+        NetAudioBean netAudioBean = new Gson().fromJson(json, NetAudioBean.class);
+        List<NetAudioBean.ListBean> datas = netAudioBean.getList();
+
+        if(datas != null && datas.size() >0){
+            //有数据
+            tvNomedia.setVisibility(View.GONE);
+            //设置适配器
+            myAdapter = new NetAudioFragmentAdapter(context,datas);
+            listview.setAdapter(myAdapter);
+        }else{
+            //没有数据
+            tvNomedia.setVisibility(View.VISIBLE);
+        }
+
+        progressbar.setVisibility(View.GONE);
     }
 
 
